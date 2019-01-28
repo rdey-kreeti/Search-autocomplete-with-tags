06ed2d34eb442e7b22fc237f092f86abd74723a3
Rajib Dey <rdey@kreeti.com>
2019-01-28 18:49:18 +0530

    creates tag using enter/comma and also implements autosuggest dropdown depending on keyword

--- src/components/search_autocomplete/index.js
+++ src/components/search_autocomplete/index.js
@@ -1,8 +1,55 @@
 import React, {Component} from 'react';
 
+import Tag from '../tag';
+
+import './styles.scss'
+
 class SearchAutocomplete extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      tags: [],
+      searchTerm: ''
+    }
+  }
+
+  handleSearch = (e) => {
+    const inputValue = e.target.value;
+    const filteredData = this.props.data.filter(item => item.name.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1);
+    if (e.keyCode === 13 || e.keyCode === 188) {
+      console.log('enter');
+      if(inputValue.trim()) {
+        this.setState({searchTerm: "", tags: [...this.state.tags, inputValue.toLowerCase().split(',')]}, () => console.log(this.state.tags));
+      }
+    } else {
+      this.setState({searchTerm: inputValue, data: filteredData});
+    }
+  }
+
+  handleRemoveTag = (removedTagIndex) => {
+    console.log(removedTagIndex);
+    const updatedTags = this.state.tags.filter((item, index) => index !== removedTagIndex);
+    this.setState({tags: updatedTags});
+  }
+
   render() {
-    return 'Search Autocomplete with tags';
+    const searchTermLength = this.state.searchTerm.trim().length;
+    return (
+      <section className="search">
+        <section className="search__input-wrapper">
+          {this.state.tags.map((item, index) => <Tag tagName={item} index={index} handleRemoveTag={this.handleRemoveTag}/>)}
+          <input type="text" placeholder="search" value={this.state.searchTerm} onChange={this.handleSearch} onKeyUp={this.handleSearch} />
+        </section>
+        { searchTermLength > 0 &&
+          <ul className="search__suggestions">
+            {this.state.data.map((item, index) => <li className="search__suggestions__item" key={index}>{item.name}</li>)}
+          </ul>
+        }
+      </section>
+
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,65 @@ import './index.css';
 import SearchAutocomplete from './components/search_autocomplete';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<SearchAutocomplete />, document.getElementById('root'));
+const GUESTS = [
+  {
+    "id": "1",
+    "name": "Sheryl Hardin"
+  },
+  {
+    "id": "2",
+    "name": "Gloria Peters"
+  },
+  {
+    "id": "3",
+    "name": "Mollie Hopper"
+  },
+  {
+    "id": "4",
+    "name": "Marcia Valentine"
+  },
+  {
+    "id": "5",
+    "name": "Leanna Stein"
+  },
+  {
+    "id": "6",
+    "name": "Albert Frederick"
+  },
+  {
+    "id": "7",
+    "name": "Gabrielle Baker"
+  },
+  {
+    "id": "8",
+    "name": "Josefina Glenn"
+  },
+  {
+    "id": "9",
+    "name": "Deanne Mason"
+  },
+  {
+    "id": "10",
+    "name": "Ilene Madden"
+  },
+  {
+    "id": "11",
+    "name": "Lourdes Patton"
+  },
+  {
+    "id": "12",
+    "name": "Hebert Hess"
+  },
+  {
+    "id": "13",
+    "name": "Latonya Burris"
+  },
+  {
+    "id": "14",
+    "name": "Courtney Camacho"
+  }
+]
+
+ReactDOM.render(<SearchAutocomplete data={GUESTS} />, document.getElementById('root'));
 
 serviceWorker.unregister();
